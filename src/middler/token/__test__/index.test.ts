f2f1124329c3624d1485d884d7cbaf8f5a7b9aa4
H-copy <1123605771@qq.com>
2022-03-31 13:03:42 +0800

    refactor: 重构token中间件实现

---
+++ src/middler/token/__test__/index.test.ts
@@ -0,0 +1,48 @@
+import axios from 'axios'
+import { CandyPaper } from '../../../core'
+import { buildReqConf, httpConf } from '../../../utils/test-mock'
+import { Token } from '../index'
+
+CandyPaper.withAxios(axios)
+
+describe('middler token', () => {
+  
+  test('固定token: xxx-xxx', async() => {
+    const http = new CandyPaper(httpConf)
+    const t = 'xxx-xxx'
+    const token = Token.create(t)
+    http.interceptor.request.use(token.adapter())
+    const { config } = await http.get('/', buildReqConf())
+    expect(config.headers).toMatchObject({token: t})
+  })
+
+  test('动态token', async () => {
+    const http = new CandyPaper(httpConf)
+    const t = 'xxx-xxx'
+    const getToken = jest.fn(() => t)
+    const token = Token.create(getToken)
+    http.interceptor.request.use(token.adapter())
+    const { config } = await http.get('/', buildReqConf())
+    expect(config.headers).toMatchObject({ token: t })
+    expect(getToken.mock.calls.length).toBe(1)
+  })
+  
+  test('自定义token绑定属性', async () => {
+    const http = new CandyPaper(httpConf)
+    const t = 'xxx-xxx'
+    const prop = 'auth'
+    const token = Token.create(t, prop)
+    http.interceptor.request.use(token.adapter())
+    const { config } = await http.get('/', buildReqConf())
+    expect(config.headers).toMatchObject({ [prop]: t })
+  })
+
+  test('install 注册函数', async () => {
+    const http = new CandyPaper(httpConf)
+    const t = 'xxx-xxx'
+    http.use(Token.create(t))
+    const { config } = await http.get('/', buildReqConf())
+    expect(config.headers).toMatchObject({ token: t })
+  })
+  
+})
--- src/middler/token/index.ts
+++ src/middler/token/index.ts
@@ -1,4 +1,5 @@
 import { AxiosRequestConfig } from 'axios'
+import { CandyPaper } from '../../core'
 import { asserts } from '../../utils'
 
 export const DEF_TOKEN_KEY = 'token'
@@ -8,32 +9,65 @@ export const DEF_TOKEN_KEY = 'token'
  * @param getToken token值或token获取函数
  * @param tokenProp token绑定属性, 默认'token'
  * @example
- * 1. 固定token
+ * 1.固定token
  * candyPaper.interceptor.request.use(
- *  'tokenId-xxx-uuu'
+ *  Token.create('tokenId:xxx-xxx-xxx').adapter()
  * )
- * 2. 动态token
- * candyPaper.interceptor.response.use(
- *  () => 'tokenId-xxx-uuu'
- * )
- * 3. 修改token挂载属性
- * candyPaper.interceptor.request.use(
- *  'token,Id-xxx-uuu',
+ * 2.动态token
+ * Token.create(() => 'tokenI:xxx-xxx-uuu').adapter()
+ * 
+ * 3.修改token挂载属性
+ * Token.create(
+ *  'tokenId:xxx-xxx-uuu',
  *  'subToken'
+ * )
+ * 
+ * 4.注册, Token对象实现了install函数
+ * candyPaper.interceptor.request.use(
+ *  Token.create('xxx-xxx-xxx')
+ * )
  */
 export type TokenFiulfilled = (ctx: AxiosRequestConfig) => AxiosRequestConfig
-export function token(getToken: string, tokenProp?: string): TokenFiulfilled
-export function token(getToken: ((ctx?: AxiosRequestConfig) => string), tokenProp?: string): TokenFiulfilled
-export function token(getToken: string | ((ctx?: AxiosRequestConfig) => string), tokenProp = 'token'): TokenFiulfilled {
-  const cb = (ctx: AxiosRequestConfig) => {
-    if(!ctx.headers){
-      ctx.headers = {}
-    }
-    ctx.headers[tokenProp] = asserts.isString(getToken) ? getToken : getToken(ctx)
-    return ctx
+export class Token {
+
+  static create(getToken: string | ((ctx?: AxiosRequestConfig) => string), tokenProp?: string):Token {
+    return new Token(getToken as any, tokenProp)
+  }
+  
+  getToken?: string | ((ctx?: AxiosRequestConfig) => string)
+  tokenProp: string = DEF_TOKEN_KEY
+
+  constructor()
+  constructor(getToken: string, tokenProp?: string)
+  constructor(getToken: ((ctx?: AxiosRequestConfig) => string), tokenProp?: string)
+  constructor(getToken?: string | ((ctx?: AxiosRequestConfig) => string), tokenProp = DEF_TOKEN_KEY){
+    this.getToken = getToken
+    this.tokenProp = tokenProp;
+  }
+  
+  install(candyPaper: CandyPaper){
+    candyPaper.interceptor.request.use(
+      this.adapter()
+    )
   }
 
-  // 默认中间件注册id
-  cb.key = DEF_TOKEN_KEY
-  return cb
+  adapter (){
+    const cb = (ctx: AxiosRequestConfig) => {
+      if (!ctx.headers) {
+        ctx.headers = {}
+      }
+
+      if (asserts.isString(this.getToken)) {
+        ctx.headers[this.tokenProp] = this.getToken
+      }
+
+      if (asserts.isFunction(this.getToken)) {
+        ctx.headers[this.tokenProp] = this.getToken(ctx)
+      }
+      return ctx
+    }
+    cb.key = this.tokenProp
+    return cb
+  }
+  
 }
